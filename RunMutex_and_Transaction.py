87df4afef70bd4286340c1a2b6608fedc0ed72bf
MikeArchbold <jazzbox35@gmail.com>
2025-12-12 11:51:27 -0800

    mutex

---
+++ RunMutex_and_Transaction.py
@@ -0,0 +1,54 @@
+import requests
+
+# This is the default IP/PORT
+url = "http://localhost:5000/metta"
+headers = {"Content-Type": "text/plain; charset=utf-8" }
+
+# Example of using mutex and transaction in order to 
+# prevent multiple threads trampling on each other.
+# MeTTa can be used as a quasi-database.
+data = """
+
+!(add-atom &temp (count 37))
+
+;This only works predictable single-threaded, else there is a data race:
+(= (sloppyinc)
+   (match &temp (count $x)
+          ((remove-atom &temp (count $x))
+           (let $inc (+ $x 1)
+                     (add-atom &temp (count $inc))))))
+
+;To protect from data reace we can use a mutex:
+(= (mutexinc)
+   (with_mutex testmutex
+               (match &temp (count $x)
+                      ((remove-atom &temp (count $x))
+                       (let $inc (+ $x 1)
+                            (add-atom &temp (count $inc)))))))
+
+;Additionally we support transactions, the following illlustrates that remove-atom is undone if the nondet branch fails:
+(= (Transaction_rollback_fail_to_inc)
+   (transaction (match &temp (count $x)
+                       ((remove-atom &temp (count $x))
+                        (let $inc (+ $x 1)
+                             (add-atom &temp (count $inc)))
+                        (empty)))))
+
+;even though swipl is threadsafe and will also not deadlock, this would cause aforementioned race condition between reading and writing:
+;!(hyperpose ((sloppyinc) (sloppyinc) (sloppyinc) (sloppyinc) (sloppyinc)))
+
+;while this is fine as all places modifying the (count $n) relation simultaneously (mutexinc only in this case) use the same mutex:
+!(hyperpose ((mutexinc) (mutexinc) (mutexinc) (mutexinc) (mutexinc)))
+!(test (collapse (get-atoms &temp)) ((count 42)))
+
+;This won't affect the count since the transaction is setup to fail:
+!(Transaction_rollback_fail_to_inc)
+!(test (collapse (get-atoms &temp)) ((count 42)))
+                    
+"""
+
+response = requests.post(url, headers=headers, data=data.encode("utf-8"))
+response.encoding = "utf-8"
+print("Status:", response.status_code)
+print("Response:", response.text)
+
